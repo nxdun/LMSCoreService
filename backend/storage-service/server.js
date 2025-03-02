bec6e6ddf5f78c4c60c44ef560529792b0f26e7d
Nadu_Dev <inbox.nadun@gmail.com>
2025-03-02 21:37:24 +0530

    chore: Update backend batch file, enhance README with storage service workflow, and refactor storage service code for improved functionality and environment variable usage

--- backend/storage-service/server.js
+++ backend/storage-service/server.js
@@ -1,109 +1,105 @@
 // Import necessary modules
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-const helmet = require('helmet');
+const cors = require("cors");
+const helmet = require("helmet");
+require("dotenv").config();
 // const ApiKeyValidator = require("./ApiKeyValidator");
 // Middleware setup
 app.use(express.json());
 app.use(cors());
-app.use(helmet()); 
+app.use(helmet());
 // app.use(ApiKeyValidator); // Validate API key
 app.use(express.urlencoded({ extended: false }));
-const axios = require('axios');
+const axios = require("axios");
 
-//cloud stoage 
-const { Storage } = require('@google-cloud/storage');
-const multer = require('multer');
+//cloud stoage
+const { Storage } = require("@google-cloud/storage");
+const multer = require("multer");
 const storage = new Storage({
-    keyFilename: './key.json'
+  keyFilename: "./key.json",
 });
 
-const bucket = storage.bucket('nxduns');
+const bucket = storage.bucket(process.env.SERVICE_NAME_BUCKET);
 
 //multer middleware
 const multerMid = multer({
-    storage: multer.memoryStorage(),
-    limits: {
-        fileSize: 5 * 1024 * 1024, // no larger than 5mb
-    },
+  storage: multer.memoryStorage(),
+  limits: {
+    fileSize: 5 * 1024 * 1024, // no larger than 5mb
+  },
 });
 
-app.get('/yo', (req, res) => {
-    //get all headers to a constant
-    const headers = req.headers;
-    res.send(headers);
+app.get("/yo", (req, res) => {
+  //get all headers to a constant
+  const headers = req.headers;
+  res.send(headers);
 });
 
 //upload file and get signed url
-app.post('/api/upload', multerMid.single('file'), (req, res) => {
-    console.log(`upload recived : ${req.file}`);
-    const file = req.file;
-    if(!file) {
-        res.status(400).send('No file uploaded.');
-        return;
+app.post("/api/upload", multerMid.single("file"), (req, res) => {
+  console.log(`upload recived : ${req.file}`);
+  const file = req.file;
+  if (!file) {
+    res.status(400).send("No file uploaded.");
+    return;
+  }
+  const fileName = Date.now() + file.originalname;
+  const blob = bucket.file(fileName);
+  const blobStream = blob.createWriteStream({
+    metadata: {
+      contentType: file.mimetype,
+    },
+  });
+
+  blobStream.on("error", (err) => {
+    res.status(500).send(err);
+  });
+
+  blobStream.on("finish", async () => {
+    //get the public signed url of the uploaded file for download
+    const file = bucket.file(fileName);
+    try {
+      const [exists] = await file.exists();
+      if (!exists) {
+        res.status(404).send("File not found");
+      }
+      const signedUrl = await file.getSignedUrl({
+        action: "read",
+        expires: "03-09-2491",
+      });
+
+      res.status(200).send(signedUrl);
+    } catch (err) {
+      res.status(500).send(err);
     }
-    const fileName = Date.now() + file.originalname;
-    const blob = bucket.file(fileName);
-    const blobStream = blob.createWriteStream({
-        metadata: {
-            contentType: file.mimetype
-        }
-    });
+  });
 
-    blobStream.on('error', (err) => {
-        res.status(500).send(err);
-    });
-
-    blobStream.on('finish',async () => {
-        //get the public signed url of the uploaded file for download
-        const file = bucket.file(fileName);
-        try{
-            const [exists] = await file.exists();
-            if(!exists){
-                res.status(404).send('File not found');
-        }
-        const signedUrl = await file.getSignedUrl({
-            action: 'read',
-            expires: '03-09-2491'
-        });
-
-        res.status(200).send(signedUrl);
-    }catch(err){
-            res.status(500).send(err);
-        }
-
-    });
-
-    //get signed url of the uploaded file
-    blobStream.end(file.buffer);
-
+  //get signed url of the uploaded file
+  blobStream.end(file.buffer);
 });
 
 //captcha
-app.post('/capcheck', async (req, res) => {
-    console.log("capcheck recived ", JSON.stringify(req.body));
-    try{
-        if(!req.body.captcha){
-            return res.status(400).json({ message: 'CapToken is required' });
-        }
-        const secretKey = "6Leg6dgpAAAAAAlzr64bqduRNTowvB-JbASCAnen";
-        const googleUrl = `https://www.google.com/recaptcha/api/siteverify?secret=${secretKey}&response=${req.body.captcha}`;
-       
-        const response = await axios.post(googleUrl);
-        if(response.data.success){
-            res.status(200).json({ message: 'CapToken is valid', success: true });
-        }else{
-            res.status(400).json({ message: 'CapToken is invalid', success: false });
-        }
+app.post("/capcheck", async (req, res) => {
+  console.log("capcheck recived ", );
+  try {
+    if (!req.body.captcha) {
+      return res.status(400).json({ message: "CapToken is required" });
+    }
+    const secretKey = process.env.SERVICE_CAPTCHA_CODE || "ENV NOT LOADED";
+    const googleUrl = `https://www.google.com/recaptcha/api/siteverify?secret=${secretKey}&response=${req.body.captcha}`;
 
+    const response = await axios.post(googleUrl);
+    if (response.data.success) {
+      res.status(200).json({ message: "CapToken is valid", success: true });
+    } else {
+      res.status(400).json({ message: "CapToken is invalid", success: false });
     }
-    catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'Internal server error', success: false});
-    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error", success: false });
+  }
 });
-
 
 const port = 2345;
 // Start the server
