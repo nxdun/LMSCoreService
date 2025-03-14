0daf1eea7e9a374edbc6907d3d87c6cf5c9cca5b
UdeeshaRukshan <IT21807480@my.sliit.lk>
2025-03-14 13:29:44 +0530

    notification microservice initialize

---
+++ backend/notify-by-udeesha/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+
+dotenv.config();
+
+const app = express();
+
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+})
