0eb92a2a24d2b6b9e15061bb382baf62744ab873
Nadu_Dev <inbox.nadun@gmail.com>
2025-03-22 14:38:27 +0530

    feat: add Open_All_Backend.bat to start API gateway and services

---
+++ Open_All_Backend.bat
@@ -0,0 +1,10 @@
+@echo off
+
+rem Start API gateway
+start cmd /k "cd .\backend\API-gateway\ & npm start"
+
+rem Start authentication service
+start cmd /k "cd .\backend\authentication-service\ & npm start"
+
+rem Start storage service
+start cmd /k "cd .\backend\storage-service\ & npm start"
